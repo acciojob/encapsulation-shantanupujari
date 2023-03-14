007152e7dd94f34b880f9c74cacbc24a4d49db77
shantanu <pujarishantanu33@gmail.com>
2023-03-14 15:50:59 +0530

    again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String []args){
       RWOnly obj = new RWOnly();
-      obj.s= 15;
-      System.out.println(obj.s);
+      //obj.s= 15; // s has private access in 'com.driver.RWOnly'
+     // System.out.println(obj.s); // s has private access in 'com.driver.RWOnly'
       obj.set(15);
       System.out.println(obj.get());
   }
