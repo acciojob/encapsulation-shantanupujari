c93ce084e2c8ae113bc3c482ddaf02aeead4aabe
shantanu <pujarishantanu33@gmail.com>
2023-03-14 15:45:15 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String []args){
+      RWOnly obj = new RWOnly();
+      obj.s= 15;
+      System.out.println(obj.s);
+      obj.set(15);
+      System.out.println(obj.get());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int s;
+     public void set(int s){
+         this.s= s;
+     }
+     public int get(){
+         return s;
+     }
 }
